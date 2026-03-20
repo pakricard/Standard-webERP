12a1a692a731c3b5b2b8c97e66efd80b7c679c20
PakRicard <pakricard@gmail.com>
2026-03-20 08:41:19 +0800

    DB changes to increase cost precision

--- sql/updates/48.php
+++ sql/updates/48.php
@@ -11,7 +11,10 @@ ChangeColumnType('lastcostrollup', 'labcost', 'decimal(24,8)', ' NOT NULL ', '')
 ChangeColumnType('lastcostrollup', 'oheadcost', 'decimal(24,8)', ' NOT NULL ', '');
 ChangeColumnType('lastcostrollup', 'newmatcost', 'decimal(24,8)', ' NOT NULL ', '');
 ChangeColumnType('lastcostrollup', 'newlabcost', 'decimal(24,8)', ' NOT NULL ', '');
-ChangeColumnType('lastcostrollup', 'newoheadcost', 'decimal(24,8)', ' NOT NULL ', '');	
+ChangeColumnType('lastcostrollup', 'newoheadcost', 'decimal(24,8)', ' NOT NULL ', '');
+
+ChangeColumnType('purchdata', 'price', 'decimal(24,8)', ' NOT NULL ', '');
+
 
 if ($_SESSION['Updates']['Errors'] == 0) {
 	UpdateDBNo(basename(__FILE__, '.php'), __('Increase precision of cost fields to 8 decimal places'), true);
