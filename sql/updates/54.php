7002c8d77c280216a3217efdbb309e5dc600a0cf
Tim Schofield <tim.schofield1960@googlemail.com>
2026-04-09 10:54:30 +0100

    Fixes https://github.com/timschofield/webERP/issues/896
    Files changed in commit:
    sql/updates/54.php

--- sql/updates/54.php
+++ sql/updates/54.php
@@ -4,7 +4,7 @@ $SQL = "SELECT userid, modulesallowed FROM www_users";
 $Result = DB_query($SQL);
 
 while ($MyRow = DB_fetch_array($Result)) {
-	if (mb_strlen($MyRow['modulesallowed']) <= 24) {
+	if (mb_strlen($MyRow['modulesallowed']) < 26) {
 		$StringLength = mb_strlen($MyRow['modulesallowed']);
 		$NewModulesAllowed = mb_substr($MyRow['modulesallowed'], 0, 16) . '1,' . mb_substr($MyRow['modulesallowed'], 16, ($StringLength - 9));
 		UpdateField('www_users', 'modulesallowed', $NewModulesAllowed, 'userid="' . $MyRow['userid'] . '"');
