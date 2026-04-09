735979445ad319fae5289d3dabc6ee6867ab1051
PakRicard <pakricard@gmail.com>
2026-04-09 17:41:59 +0800

    Adds the new module Human Resources correctly to existing users

--- sql/updates/54.php
+++ sql/updates/54.php
@@ -4,9 +4,9 @@ $SQL = "SELECT userid, modulesallowed FROM www_users";
 $Result = DB_query($SQL);
 
 while ($MyRow = DB_fetch_array($Result)) {
-	if (mb_strlen($MyRow['modulesallowed']) < 24) {
+	if (mb_strlen($MyRow['modulesallowed']) <= 24) {
 		$StringLength = mb_strlen($MyRow['modulesallowed']);
-		$NewModulesAllowed = mb_substr($MyRow['modulesallowed'], 0, 14) . '1,' . mb_substr($MyRow['modulesallowed'], 14, ($StringLength - 11));
+		$NewModulesAllowed = mb_substr($MyRow['modulesallowed'], 0, 16) . '1,' . mb_substr($MyRow['modulesallowed'], 16, ($StringLength - 9));
 		UpdateField('www_users', 'modulesallowed', $NewModulesAllowed, 'userid="' . $MyRow['userid'] . '"');
 	}
 }
